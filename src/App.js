89a939b0fc836e1575e1901c60b63662aff96e45
Daniel Malesevic <daniel.malesevic@gmx.at>
2020-01-08 17:22:39 +0000

    Add basic setup

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <h1>Compare your Air</h1>
+      <p>Compare the air quality between cities in the UK.</p>
+      <p>Seelct cities to compare using the search tool below.</p>
     </div>
   );
 }
---
+++ src/components/SearchBar.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export const SearchBar = props => {
+
+    return (
+        <input />
+    )
+}
