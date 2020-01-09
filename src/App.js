fcdc3e8a6b085acd4e6e9a4e7b1c111157327e3b
Daniel Malesevic <daniel.malesevic@gmx.at>
2020-01-09 09:45:27 +0000

    Add SearchBar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import { SearchBar } from "./components/SearchBar";
 
 function App() {
   return (
     <div className="App">
       <h1>Compare your Air</h1>
-      <p>Compare the air quality between cities in the UK.</p>
-      <p>Seelct cities to compare using the search tool below.</p>
+      <div className="my-4">
+        <p>Compare the air quality between cities in the UK.</p>
+        <p>Select cities to compare using the search tool below.</p>
+      </div>
+      <SearchBar />
     </div>
   );
 }
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,8 +1,53 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "../styles/SearchBar.css";
 
 export const SearchBar = props => {
+  const [locations, setLocations] = useState([]);
+  const [results, setResults] = useState([]);
+  const [searchText, setText] = useState("");
 
-    return (
-        <input />
-    )
-}
+  // initally fetch all locations from api as there is no search endpoint
+  useEffect(() => {
+    const fetchLocations = async () => {
+      const res = await axios.get(
+        "https://api.openaq.org/v1/locations?country=GB&limit=10000"
+      );
+      setLocations(res.data.results);
+    };
+    fetchLocations();
+  }, []);
+
+  // update results every time the user changes the search text
+  useEffect(() => {
+    searchText.length > 0
+      ? setResults(
+          locations.filter(location =>
+            location.location.toLowerCase().includes(searchText.toLowerCase())
+          )
+        )
+      : setResults([]);
+  }, [searchText]);
+
+  return (
+    <div className="my-5 mx-auto search-container">
+      <input
+        type="search"
+        className="search-bar"
+        placeholder="Enter city name..."
+        value={searchText}
+        onChange={ev => setText(ev.target.value)}
+      />
+      {searchText.length > 0 &&
+      <div className="result-container">
+        {results.map(result => (
+            <div className="result-option">
+                {result.location}
+            </div>
+        ))}
+      </div>
+      }
+      
+    </div>
+  );
+};
